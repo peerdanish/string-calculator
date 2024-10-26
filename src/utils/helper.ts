b91e25cb820c966f4cd8915e1154499546b8b8e6
Danish Peer <peerdanish71@gmail.com>
2024-10-26 16:48:54 +0530

    added more cases to handle multiple delimiters & Ignore values > 1000

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,12 +1,17 @@
-import { DEFAULT_DELIMITER, DELIMITER, ERROR_MESSAGE } from "../global/constants";
+import {
+  DEFAULT_DELIMITER,
+  DELIMITER,
+  ERROR_MESSAGE,
+} from "@/global/constants";
 
 export const add = (numbers: string) => {
   if (!numbers) return 0;
   numbers = numbers.replace(/\\n/g, "\n");
   let delimiter = DEFAULT_DELIMITER;
+
   if (numbers.startsWith(DELIMITER.CUSTOM_DELIMITER)) {
     const parts = numbers.split(DELIMITER.NEWLINE);
-    delimiter = new RegExp(parts[0].slice(2) + '|'+ DELIMITER.NEWLINE);
+    delimiter = new RegExp(extractDelimiters(parts[0]));
     numbers = numbers.slice(parts[0].length + 1);
   }
 
@@ -17,10 +22,23 @@ export const add = (numbers: string) => {
       `${ERROR_MESSAGE.NEGATIVE_INPUT} ${negatives.join(DELIMITER.COMMA)}`
     );
   }
-  const sum = nums.reduce((sum, num) => sum + num, 0);
+  const sum = nums.reduce((sum, num) => {
+    if (num > 1000) {
+      return sum;
+    }
+    return sum + num;
+  }, 0);
+
   if (isNaN(sum)) {
     throw new Error(ERROR_MESSAGE.INVALID_INPUT);
   }
 
   return sum;
+};
+
+const extractDelimiters = (numbers: string) => {
+  const myvar = numbers.slice(2);
+  const delimiterArr = myvar.split(/[\\[\]]/).filter((item) => item !== "");
+  const delimiters = "[" + delimiterArr.join("|") + `]|${DELIMITER.NEWLINE}`;
+  return delimiters;
 };
